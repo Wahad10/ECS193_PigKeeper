e399d55fef53518ef39c036e654163250cf71b99
Wahad10 <walatif@ucdavis.edu>
2024-02-09 11:38:28 -0800

    Created New Players Screen which accepts player name inputs and transitions to Turn Order Roll Screen.  Still Need to decide what data structure to use to save names across multiple screens, for now using String array

--- app/src/main/java/com/example/pigkeeper/NewPlayersActivity.kt
+++ app/src/main/java/com/example/pigkeeper/NewPlayersActivity.kt
@@ -1,11 +1,34 @@
 package com.example.pigkeeper
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
 
 class NewPlayersActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_players)
+
+        val namesArray = ArrayList<String>()
+
+        val buttonStartRound = findViewById<Button>(R.id.buttonStartRound)
+        buttonStartRound.setOnClickListener{
+            println("here")
+            for(name in namesArray){
+                println(name)
+            }
+            startActivity(Intent(this@NewPlayersActivity, TurnOrderRollActivity::class.java))
+        }
+
+        val inputName = findViewById<EditText>(R.id.inputName)
+
+        val buttonNextPlayer = findViewById<Button>(R.id.buttonNextPlayer)
+        buttonNextPlayer.setOnClickListener {
+            namesArray.add(inputName.getText().toString())
+            inputName.getText().clear()
+        }
     }
 }
