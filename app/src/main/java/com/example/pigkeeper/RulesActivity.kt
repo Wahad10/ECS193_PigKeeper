fff4b901d836b78a13689fab5b85d821ab9b8a8a
khaiberamin1 <157650486+khaiberamin1@users.noreply.github.com>
2024-02-15 10:31:57 -0800

    Update RulesActivity.kt
    Added "Back" button functionality.

--- app/src/main/java/com/example/pigkeeper/RulesActivity.kt
+++ app/src/main/java/com/example/pigkeeper/RulesActivity.kt
@@ -7,5 +7,8 @@ class RulesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rules)
+
+        val buttonBack = findViewById<Button>(R.id.buttonBack)
+        buttonBack.setOnClickListener{startActivity(Intent(this@RulesActivity, MainActivity::class.java))}
     }
 }
