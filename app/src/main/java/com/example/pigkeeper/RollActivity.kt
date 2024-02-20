8a827579fc8531dea3087bdbf4821d0e250cde48
Wahad10 <walatif@ucdavis.edu>
2024-02-19 22:38:30 -0800

    updated new players screen and roll screens

--- app/src/main/java/com/example/pigkeeper/RollActivity.kt
+++ app/src/main/java/com/example/pigkeeper/RollActivity.kt
@@ -3,9 +3,9 @@ package com.example.pigkeeper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class TurnOrderRollActivity : AppCompatActivity() {
+class RollActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_turn_order_roll)
+        setContentView(R.layout.activity_roll)
     }
 }
