54b2665c416a01ffee8c9e3cd8204e4808636fcc
Wahad10 <walatif@ucdavis.edu>
2024-02-20 23:06:10 -0800

    basic functionality of roll activity added, no special rules or error checking yet, no win condition yet

--- app/src/main/java/com/example/pigkeeper/RollActivity.kt
+++ app/src/main/java/com/example/pigkeeper/RollActivity.kt
@@ -1,8 +1,11 @@
 package com.example.pigkeeper
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Intent
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -11,10 +14,24 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 
 class RollActivity : AppCompatActivity() {
+    //Necessary data from other screens
+    private val namesArray = ArrayList<String>()
+    private var nameToScore = HashMap<String, Int>()
+
+    //Keep track of current roll screen
     private var selectedLeftDice: Int = 0
     private var selectedRightDice: Int = 0
-    private var selectedScore: Int = 0
+    private var selectedBadRoll: Boolean = false
     private var currentPlayer: String = ""
+    private var currentPlayerNewScore: Int = 0
+    private var lastPlayer: String = ""
+    private var lastPlayerLastScore: Int = 0
+
+    //Items from layout that need to be passed to other functions
+    private lateinit var textYourScore: TextView
+    private lateinit var textTopScore: TextView
+    private lateinit var leftDiceButtons: List<ImageButton>
+    private lateinit var rightDiceButtons: List<ImageButton>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +39,32 @@ class RollActivity : AppCompatActivity() {
         setContentView(R.layout.activity_roll)
 
         val globalVariable = GlobalData.instance
+
         val players = globalVariable.players
         val sittingOut = globalVariable.sittingOut
+        for(player in players){
+            if(sittingOut[player] == false){
+                namesArray.add(player)
+            }
+        }
+        currentPlayer = namesArray[0]
 
+        //HAVE TO RECONCILE THESE BOTTOM TWO DATA STRUCTURES, CHANGE GLOBAL VARIABLE SCORE TO HASHMAP
         //Idk if you need this
         val score = globalVariable.score
+        //hashmap holds player names and records their scores
+        for (name in namesArray){
+            nameToScore[name] = 0
+        }
 
 
-        val textYourScore = findViewById<TextView>(R.id.textYourScore)
-        val textTopScore = findViewById<TextView>(R.id.textTopScore)
-
+        //Get all items from layout
+        textYourScore = findViewById<TextView>(R.id.textYourScore)
+        textTopScore = findViewById<TextView>(R.id.textTopScore)
 
         val topCard = findViewById<CardView>(R.id.cardTop)
-        topCard.setOnClickListener{startActivity(Intent(this@RollActivity, MainActivity::class.java))}
 
-
-        val leftDiceButtons = listOf<ImageButton>(
+        leftDiceButtons = listOf<ImageButton>(
             findViewById(R.id.imageButtonL1),
             findViewById(R.id.imageButtonL2),
             findViewById(R.id.imageButtonL3),
@@ -46,8 +73,7 @@ class RollActivity : AppCompatActivity() {
             findViewById(R.id.imageButtonL6)
         )
 
-
-        val rightDiceButtons = listOf<ImageButton>(
+         rightDiceButtons = listOf<ImageButton>(
             findViewById(R.id.imageButtonR1),
             findViewById(R.id.imageButtonR2),
             findViewById(R.id.imageButtonR3),
@@ -56,36 +82,21 @@ class RollActivity : AppCompatActivity() {
             findViewById(R.id.imageButtonR6)
         )
 
-
         val badRoll = findViewById<ImageButton>(R.id.imageButtonBadRoll)
         val undoRoll = findViewById<ImageButton>(R.id.imageButtonUndoRoll)
         val nextRoll = findViewById<ImageButton>(R.id.imageButtonNextRoll)
 
 
-        //need to modify this to take in the list of players in the right turn order from turn order screen
-        val namesArray = ArrayList<String>()
-        namesArray.add("Jason")
-        namesArray.add("Tim")
-        namesArray.add("Sam")
-        currentPlayer = namesArray[0]
+        //When you click on the scores card, It will take you to All Scores screen
+        topCard.setOnClickListener{startActivity(Intent(this@RollActivity, MainActivity::class.java))}
 
-
-        var nameToScore = HashMap<String, Int>()
-        for (name in namesArray){
-            nameToScore[name] = 0
-        }
-
-
-
-
+        //When you click on the dice, it sets the selected dice, updates the dice button and current player score
         for (i in 1..6) {
             leftDiceButtons[i - 1].setOnClickListener {
-                print("here?")
                 selectedLeftDice = i
                 updateDiceButtonSelection(leftDiceButtons, i)
                 updateScore()
             }
-
 
             rightDiceButtons[i - 1].setOnClickListener {
                 selectedRightDice = i
@@ -94,35 +105,93 @@ class RollActivity : AppCompatActivity() {
             }
         }
 
+        //When you click bad roll it sets the flag variable
+        badRoll.setOnClickListener {
+            selectedBadRoll = true
+        }
 
-        /**nextRoll.setOnClickListener {
-        nameToScore[currentPlayer] = selectedScore
+        //When you click next roll it saves the current player's score and loads next player
+        nextRoll.setOnClickListener {
+            rollNext()
+        }
 
+        //When you click undo roll it loads last player and erases their last roll
+        undoRoll.setOnClickListener {
+            rollUndo()
+        }
+    }
+
+    //need to make this function change the dice buttons somehow, maybe highlight them?
+    //right now just changing color of whole image to yellow
+    private fun updateDiceButtonSelection(buttons: List<ImageButton>, selectedDice: Int) {
+        /**for (button in buttons) {
+            button.isPressed = false
+        }
+        buttons[selectedDice - 1].isPressed = true*/
+
+        for (button in buttons) {
+            button.setColorFilter(null) // Reset color filter for all buttons
+        }
+
+        if(selectedDice > 0) {
+            buttons[selectedDice - 1].setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP) // Change color of selected button
+        }
+    }
+
+    //main logic function to calculate the players score based on the two dice
+    private fun updateScore() {
+        var selectedScore = selectedLeftDice + selectedRightDice
+        currentPlayerNewScore = nameToScore[currentPlayer]!! + selectedScore
+
+        //need to implement special rule cases here
+
+        textYourScore.text = "YOUR SCORE:\n$currentPlayer: $currentPlayerNewScore"
+
+        //update top score
+        val topScore = nameToScore.values.maxOrNull()!!
+        val topPlayer = nameToScore.entries.firstOrNull { it.value == topScore }?.key
+        textTopScore.text = "TOP SCORE:\n$topPlayer: $topScore"
+    }
+
+    //saves the current player's info and loads next player
+    private fun rollNext(){
+        //save current player's info
+        lastPlayer = currentPlayer
+        lastPlayerLastScore = nameToScore[currentPlayer]!!
+
+        //update current player's score
+        nameToScore[currentPlayer] = currentPlayerNewScore
 
         //find next player in namesArray and save it to currentPlayer
         val currentPlayerIndex = namesArray.indexOf(currentPlayer)
         currentPlayer = if (currentPlayerIndex < namesArray.size - 1) {
-        namesArray[currentPlayerIndex + 1]
+            namesArray[currentPlayerIndex + 1]
         } else {
-        namesArray[0]
+            namesArray[0]
         }
-        selectedScore = 0
+
+        currentPlayerNewScore = 0
+        selectedLeftDice = 0
+        selectedRightDice = 0
+        selectedBadRoll = false
+
         updateScore()
-        }**/
+        updateDiceButtonSelection(leftDiceButtons, -1)
+        updateDiceButtonSelection(rightDiceButtons, -1)
     }
 
+    //loads last player and erases their last roll
+    private fun rollUndo(){
+        currentPlayer = lastPlayer
+        nameToScore[currentPlayer] = lastPlayerLastScore
 
-    private fun updateDiceButtonSelection(buttons: List<ImageButton>, selectedDice: Int) {
-        for (button in buttons) {
-            button.isPressed = false
-        }
-        buttons[selectedDice - 1].isPressed = true
-    }
+        currentPlayerNewScore = 0
+        selectedLeftDice = 0
+        selectedRightDice = 0
+        selectedBadRoll = false
 
-
-    private fun updateScore() {
-        selectedScore = selectedLeftDice + selectedRightDice
-        Log.d("me", "here")
-        Log.d("me", selectedScore.toString())
+        updateScore()
+        updateDiceButtonSelection(leftDiceButtons, -1)
+        updateDiceButtonSelection(rightDiceButtons, -1)
     }
 }
