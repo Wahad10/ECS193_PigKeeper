2f476d5c57bd79364215538093e219df55251cb8
khaiberamin1 <157650486+khaiberamin1@users.noreply.github.com>
2024-02-22 11:18:34 -0800

    Update RulesActivity.kt

--- app/src/main/java/com/example/pigkeeper/RulesActivity.kt
+++ app/src/main/java/com/example/pigkeeper/RulesActivity.kt
@@ -10,6 +10,8 @@ import android.graphics.drawable.ColorDrawable
 import android.media.session.PlaybackState.CustomAction
 import android.view.View
 import android.widget.ToggleButton
+//import java.lang.Error
+import kotlin.Error
 
 class RulesActivity : AppCompatActivity() {
 
@@ -61,6 +63,7 @@ class RulesActivity : AppCompatActivity() {
         val buttonNoEffect = findViewById<Button>(R.id.buttonNoEffect)
 
         //List of SpecialRuleCase enums. This list is data that passes through application
+        //REPLACE WITH MAP. SRCs WILL BE THE KEY, LIST OF CONSEQEUNCES WILL BE THE VALUES
         val SpecialRuleCaseList: List<SpecialRuleCase> = listOf(
             SpecialRuleCase.ROLL_7,
             SpecialRuleCase.SNAKE_EYES,
@@ -102,9 +105,153 @@ class RulesActivity : AppCompatActivity() {
             }
         }
 
+        //SCAFFOLDING TOP------------------------
+
+        //create Map, where SPCs are keys and list of consequences are value
+        val rulesMap: MutableMap<SpecialRuleCase, MutableList<Consequence>> = mutableMapOf()
+        rulesMap[SpecialRuleCase.ROLL_7] = mutableListOf(Consequence.LOSE_TURN)
+        rulesMap[SpecialRuleCase.SNAKE_EYES] = mutableListOf(Consequence.RESET_SCORE_TO_ZERO)
+        rulesMap[SpecialRuleCase.BOX_CARS] = mutableListOf(Consequence.NO_EFFECT)
+        rulesMap[SpecialRuleCase.DOUBLE] = mutableListOf(Consequence.DOUBLE_POINTS, Consequence.MUST_ROLL_AGAIN)
+        rulesMap[SpecialRuleCase.TRIPLE_DOUBLE] = mutableListOf(Consequence.RESET_SCORE_TO_ZERO)
+        rulesMap[SpecialRuleCase.OFF_TABLE] = mutableListOf(Consequence.LOSE_TURN)
+
+        var currentlySelectedSpecialRuleCaseCode = 0
+        var IsAnySpecialRuleCaseCurrentlySelected = false
+
+
+        fun getMapKey(code: Int): SpecialRuleCase {
+            return when (code) {
+                1 -> SpecialRuleCase.ROLL_7
+                2 -> SpecialRuleCase.SNAKE_EYES
+                3 -> SpecialRuleCase.BOX_CARS
+                4 -> SpecialRuleCase.DOUBLE
+                5 -> SpecialRuleCase.TRIPLE_DOUBLE
+                else -> SpecialRuleCase.OFF_TABLE
+            }
+        }
+
+        //var currentSpecialRoleCase = getMapKey(currentlySelectedSpecialRuleCaseCode)
+        var currentSpecialRoleCase = SpecialRuleCase.ROLL_7 //initialized
+//        println("currentSpecialRoleCase.listOfConsequence:")
+//        println(currentSpecialRoleCase.listOfConsequence)
+//        println("rulesMap[SpecialRuleCase.DOUBLE]:")
+//        println(rulesMap[SpecialRuleCase.DOUBLE])
+//        rulesMap[SpecialRuleCase.DOUBLE]?.add(Consequence.RESET_TURN_SCORE)
+//        println(rulesMap[SpecialRuleCase.DOUBLE])
+
+        fun printAllTable() {
+            println("ROLL_7: " + rulesMap[SpecialRuleCase.ROLL_7])
+            println("SNAKE_EYES: " + rulesMap[SpecialRuleCase.SNAKE_EYES])
+            println("BOX_CARS: " + rulesMap[SpecialRuleCase.BOX_CARS])
+            println("DOUBLE: " + rulesMap[SpecialRuleCase.DOUBLE])
+            println("TRIPLE_DOUBLE: " + rulesMap[SpecialRuleCase.TRIPLE_DOUBLE])
+            println("OFF_TABLE: " + rulesMap[SpecialRuleCase.OFF_TABLE])
+            println("-----------------------")
+        }
+
+        printAllTable()
+
+        var _2xPointsIsActive = false
+        var LoseTurnIsActive = false
+        var MustRollAgainIsActive = false
+        var ResetTurnScoreIsActive = false
+        var ResetScoreTo0IsActive = false
+        var NoEffectIsActive = false
+
+        button2XPoints.setOnClickListener {
+            _2xPointsIsActive = !_2xPointsIsActive //toggle isActive bool
+
+            if (_2xPointsIsActive == false) {
+                if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.DOUBLE_POINTS) == true) {
+                    rulesMap[currentSpecialRoleCase]?.remove(Consequence.DOUBLE_POINTS)
+                }
+            }
+            else if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.DOUBLE_POINTS) == false) {
+                rulesMap[currentSpecialRoleCase]?.add(Consequence.DOUBLE_POINTS)
+            }
+
+            printAllTable() //for testing output
+
+        }
+
+        buttonLoseTurn.setOnClickListener {
+            LoseTurnIsActive = !LoseTurnIsActive
+
+            if (LoseTurnIsActive == false) {
+                if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.LOSE_TURN) == true) {
+                    rulesMap[currentSpecialRoleCase]?.remove(Consequence.LOSE_TURN)
+                }
+            }
+            else if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.LOSE_TURN) == false) {
+                rulesMap[currentSpecialRoleCase]?.add(Consequence.LOSE_TURN)
+            }
+            printAllTable() //for testing output
+        }
+
+        buttonRollAgain.setOnClickListener {
+            MustRollAgainIsActive = !MustRollAgainIsActive
+
+            if (MustRollAgainIsActive == false) {
+                if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.MUST_ROLL_AGAIN) == true) {
+                    rulesMap[currentSpecialRoleCase]?.remove(Consequence.MUST_ROLL_AGAIN)
+                }
+            }
+            else if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.MUST_ROLL_AGAIN) == false) {
+                rulesMap[currentSpecialRoleCase]?.add(Consequence.MUST_ROLL_AGAIN)
+            }
+            printAllTable() //for testing output
+        }
+
+        buttonResetTurnScore.setOnClickListener {
+            ResetTurnScoreIsActive = !ResetScoreTo0IsActive
+
+            if (ResetTurnScoreIsActive == false) {
+                if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.RESET_TURN_SCORE) == true) {
+                    rulesMap[currentSpecialRoleCase]?.remove(Consequence.RESET_TURN_SCORE)
+                }
+            }
+            else if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.RESET_TURN_SCORE) == false) {
+                rulesMap[currentSpecialRoleCase]?.add(Consequence.RESET_TURN_SCORE)
+            }
+            printAllTable() //for testing output
+        }
+
+        buttonResetScoreToO.setOnClickListener {
+            ResetScoreTo0IsActive = !ResetScoreTo0IsActive
+
+            if (ResetScoreTo0IsActive == false) {
+                if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.RESET_SCORE_TO_ZERO) == true) {
+                    rulesMap[currentSpecialRoleCase]?.remove(Consequence.RESET_SCORE_TO_ZERO)
+                }
+            }
+            else if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.RESET_SCORE_TO_ZERO) == false) {
+                rulesMap[currentSpecialRoleCase]?.add(Consequence.RESET_SCORE_TO_ZERO)
+            }
+            printAllTable() //for testing output
+        }
+
+        buttonNoEffect.setOnClickListener {
+            NoEffectIsActive = !NoEffectIsActive
+
+            if (NoEffectIsActive == false) {
+                if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.NO_EFFECT) == true) {
+                    rulesMap[currentSpecialRoleCase]?.remove(Consequence.NO_EFFECT)
+                }
+            }
+            else if (rulesMap[currentSpecialRoleCase]?.contains(Consequence.NO_EFFECT) == false) {
+                rulesMap[currentSpecialRoleCase]?.add(Consequence.NO_EFFECT)
+            }
+            printAllTable() //for testing output
+        }
+
+        //SCAFFOLDING BOTTOM-------------------------
+
         buttonRoll7.setOnClickListener {
             undoLastClick(resetInt)
             resetInt = 1
+            //currentlySelectedSpecialRuleCaseCode = 1
+            currentSpecialRoleCase = SpecialRuleCase.ROLL_7
             toggleButton(buttonRoll7)
             toggleButton(buttonLoseTurn)
 
@@ -112,18 +259,24 @@ class RulesActivity : AppCompatActivity() {
         buttonSnakeEyes.setOnClickListener {
             undoLastClick(resetInt)
             resetInt = 2
+            //currentlySelectedSpecialRuleCaseCode = 2
+            currentSpecialRoleCase = SpecialRuleCase.SNAKE_EYES
             toggleButton(buttonSnakeEyes)
             toggleButton(buttonResetScoreToO)
         }
         buttonBoxCars.setOnClickListener {
             undoLastClick(resetInt)
             resetInt = 3
+            //currentlySelectedSpecialRuleCaseCode = 3
+            currentSpecialRoleCase = SpecialRuleCase.BOX_CARS
             toggleButton(buttonBoxCars)
             toggleButton(buttonNoEffect)
         }
         buttonDouble.setOnClickListener {
             undoLastClick(resetInt)
             resetInt = 4
+            //currentlySelectedSpecialRuleCaseCode = 4
+            currentSpecialRoleCase = SpecialRuleCase.DOUBLE
             toggleButton(buttonDouble)
             toggleButton(button2XPoints)
             toggleButton(buttonRollAgain)
@@ -131,12 +284,16 @@ class RulesActivity : AppCompatActivity() {
         button3xDoubles.setOnClickListener {
             undoLastClick(resetInt)
             resetInt = 5
+            //currentlySelectedSpecialRuleCaseCode = 5
+            currentSpecialRoleCase = SpecialRuleCase.TRIPLE_DOUBLE
             toggleButton(button3xDoubles)
             toggleButton(buttonResetScoreToO)
         }
         buttonOffTable.setOnClickListener {
             undoLastClick(resetInt)
             resetInt = 6
+            //currentlySelectedSpecialRuleCaseCode = 6
+            currentSpecialRoleCase = SpecialRuleCase.OFF_TABLE
             toggleButton(buttonOffTable)
             toggleButton(buttonLoseTurn)
         }
@@ -146,5 +303,4 @@ class RulesActivity : AppCompatActivity() {
     fun onSpecialTurnCaseClick(view: View) {
         specialTurnCaseSelected = !specialTurnCaseSelected
     }
-
 }
