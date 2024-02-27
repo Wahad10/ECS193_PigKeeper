4c44152093af09d0cc2d5797357ded6dfedad760
Stephen <sndonecker@ucdavis.edu>
2024-02-26 18:35:20 -0800

    Added a globalData class and the ability to set and get data like players,sittingOut,Score

--- app/src/main/java/com/example/pigkeeper/TurnOrderActivity.kt
+++ app/src/main/java/com/example/pigkeeper/TurnOrderActivity.kt
@@ -5,8 +5,10 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
@@ -14,17 +16,20 @@ import kotlin.math.ceil
 
 class TurnOrderActivity : AppCompatActivity() {
     var sitOut = false
-
-    lateinit var players: ArrayList<String> //listOf("Jason","Sam","Peter","Luck","Avery","May","Tim","Judith","Garen","Donny")
+    lateinit var players: ArrayList<String>
 
     //Maps a players name to whether they are sitting out
     var sittingOut = mutableMapOf<String, Boolean>()
+    var sittingOutSize = 0
 
     //This is the turn order
     var order = ArrayList<String>()
     lateinit var buttonSitOut : Button
 
     val globalVariable = GlobalData.instance
+    lateinit var message1 : TextView
+    lateinit var message2 : TextView
+    lateinit var message3 : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,19 +42,25 @@ class TurnOrderActivity : AppCompatActivity() {
         buttonSitOut.setOnClickListener{
             sitOut = !sitOut
 
-            if(sitOut){
-                buttonSitOut.text = "Set Order"
-            }
-            else{
-                buttonSitOut.text = "Sit Out"
-            }
+            if(sitOut){ buttonSitOut.text = "Set Order" }
+            else{ buttonSitOut.text = "Sit Out" }
         }
+
+        message1 = findViewById<TextView>(R.id.Message3)
+        message2 = findViewById<TextView>(R.id.Message3)
+        message3 = findViewById<TextView>(R.id.Message3)
 
         val buttonStart = findViewById<Button>(R.id.buttonStart)
         buttonStart.setOnClickListener{
-            globalVariable.players = order
-            globalVariable.sittingOut = sittingOut
-            startActivity(Intent(this@TurnOrderActivity, RollActivity::class.java))
+            if(order.size+sittingOutSize!=players.size){
+                message3.visibility=View.VISIBLE
+            }
+            else{
+                globalVariable.players = order
+                globalVariable.sittingOut = sittingOut
+                startActivity(Intent(this@TurnOrderActivity, RollActivity::class.java))
+            }
+
         }
 
         val unSelect = findViewById<ImageView>(R.id.unSelect)
@@ -89,25 +100,16 @@ class TurnOrderActivity : AppCompatActivity() {
                 button.setBackgroundTintList(ColorStateList.valueOf(Color.LTGRAY))
 
                 button.setOnClickListener{
-                    if(sitOut){
-                        setSitOut(button)
-                    }
-                    else {
-                        setOrder(button)
-                    }
+                    if(sitOut){ setSitOut(button) }
+                    else { setOrder(button) }
                 }
-
                 layout.addView(button)
                 id++
 
                 val constraintSet = ConstraintSet()
                 constraintSet.clone(layout)
-
                 constraintSet.connect(button.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, horizontalSpacing + c * (gapWidth + horizontalSpacing)+250)
-                //constraintSet.connect(button.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, horizontalSpacing + c * (buttonWidth + horizontalSpacing) + buttonWidth)
                 constraintSet.connect(button.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, verticalSpacing + r * (gapHeight + verticalSpacing)+700)
-                //constraintSet.connect(button.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, verticalSpacing + r * (buttonHeight + verticalSpacing) + buttonHeight)
-
                 constraintSet.applyTo(layout)
             }
         }
@@ -115,8 +117,16 @@ class TurnOrderActivity : AppCompatActivity() {
 
     //Will allow them to select the turn order
     private fun setOrder(button : Button){
+        //Return if they already have an order OR they are sitting out
         if(order.contains(button.tag)){return}
+        if(sittingOut[button.tag]==true){
+            message1.visibility = View.VISIBLE
+            return
+        }
 
+        message1.visibility = View.INVISIBLE
+        message2.visibility = View.INVISIBLE
+        message3.visibility = View.INVISIBLE
         order.add(button.tag as String)
         button.text = "${order.size} ${button.tag}"
 
@@ -126,13 +136,23 @@ class TurnOrderActivity : AppCompatActivity() {
     //Toggles person's sitting out status. Will also change the color of button
     //Status is found in the SittingOut Map
     private fun setSitOut(button : Button){
+        if(order.contains(button.tag)){
+            message2.visibility = View.VISIBLE
+            return
+        }
+
+        message1.visibility = View.INVISIBLE
+        message2.visibility = View.INVISIBLE
+        message3.visibility = View.INVISIBLE
         if(sittingOut[button.tag]==false){
             button.setBackgroundTintList(ColorStateList.valueOf(Color.DKGRAY))
             sittingOut[button.tag as String] = true
+            sittingOutSize+=1
         }
         else{
             button.setBackgroundTintList(ColorStateList.valueOf(Color.LTGRAY))
             sittingOut[button.tag as String] = false
+            sittingOutSize-=1
         }
     }
 }
